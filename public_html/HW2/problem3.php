d784b230a062efd25a03972edfeb2804e04f69ae
aa2789 <aa2789@njit.edu>
2021-09-18 15:25:51 -0400

    done with problem 3

--- public_html/HW2/problem3.php
+++ public_html/HW2/problem3.php
@@ -8,7 +8,12 @@ function bePositive($arr) {
     echo "<br>Processing Array:<br><pre>" . var_export($arr, true) . "</pre>";
     echo "<br>Positive output:<br>";
     //TODO use echo to output all of the values as positive (even if they were originally positive)
-
+    for($x=0;$x<sizeof($arr);$x++){
+        if($x>0){
+            echo ", ";
+        }
+        echo(abs($arr[$x]));
+    }
 }
 echo "Problem 3: Be Positive<br>";
 ?>
